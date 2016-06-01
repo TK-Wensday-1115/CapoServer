b1711199f172a144f6b9b5299a14d6151f84614f
mkasprz <mkasprz@student.agh.edu.pl>
2016-06-01 10:42:20 +0200

    Added 'PieChart' and 'HistoryChart'

--- src/main/java/pl/edu/agh/toik/Controller.java
+++ src/main/java/pl/edu/agh/toik/Controller.java
@@ -1,9 +1,13 @@
 package pl.edu.agh.toik;
 
+import javafx.embed.swing.SwingNode;
 import javafx.fxml.FXML;
+import pl.edu.agh.piechart.PieChartPanel;
 import pl.edu.agh.student.mkasprz.tk.chart3.SimpleTable;
 import pl.edu.agh.student.smialek.tk.communications.server.CommunicationsServer;
 import pl.edu.agh.student.smialek.tk.communications.server.SensorReading;
+import pl.edu.agh.toik.historychart.HistoryChartFactory;
+import pl.edu.agh.toik.historychart.TimeUnit;
 
 import java.io.PrintStream;
 import java.time.ZoneId;
@@ -16,7 +20,16 @@ public class Controller {
     private SimpleTable simpleTable;
 
     @FXML
+    private SwingNode pieChartPanel;
+
+    @FXML
+    private SwingNode historyChart;
+
+    @FXML
     void initialize() {
+        pieChartPanel.setContent(new PieChartPanel("TITLE"));
+        historyChart.setContent(HistoryChartFactory.createNew("NAME", "X", TimeUnit.Second, "Y", "y"));
+
         CommunicationsServer.registerCallback(this::printReading);
         System.out.println("Callback registered.");
 
